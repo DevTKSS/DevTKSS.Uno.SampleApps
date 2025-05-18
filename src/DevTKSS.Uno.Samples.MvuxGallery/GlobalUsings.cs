db72dcb41b43015e048820250d4d6f867c875d41
DevTKSS <info@technische-konstruktion.com>
2025-05-18 18:19:27 +0200

    chore: Clean up csproj chore: Add global using

--- src/DevTKSS.Uno.Samples.MvuxGallery/GlobalUsings.cs
+++ src/DevTKSS.Uno.Samples.MvuxGallery/GlobalUsings.cs
@@ -1,17 +1,18 @@
 global using System.Collections.Immutable;
+global using System.Text.Json.Serialization;
+global using DevTKSS.Extensions.Uno.Storage;
+global using DevTKSS.Uno.Samples.MvuxGallery.Models;
+global using DevTKSS.Uno.Samples.MvuxGallery.Models.CodeSamples;
+global using DevTKSS.Uno.Samples.MvuxGallery.Models.GalleryImages;
+global using DevTKSS.Uno.Samples.MvuxGallery.Presentation;
+global using DevTKSS.Uno.Samples.MvuxGallery.Presentation.ViewModels;
+global using DevTKSS.Uno.Samples.MvuxGallery.Presentation.Views;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Localization;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
-global using DevTKSS.Uno.Samples.MvuxGallery.Models;
-global using DevTKSS.Uno.Samples.MvuxGallery.Presentation;
-global using DevTKSS.Uno.Samples.MvuxGallery.Presentation.ViewModels;
-global using DevTKSS.Uno.Samples.MvuxGallery.Presentation.Views;
-global using DevTKSS.Uno.Samples.MvuxGallery.Models.CodeSamples;
-global using DevTKSS.Uno.Samples.MvuxGallery.Models.GalleryImages;
 global using Microsoft.UI.Xaml.Data;
-global using System.Text.Json.Serialization;
-global using DevTKSS.Extensions.Uno.Storage;
+global using Uno.Extensions.Reactive.Commands;
 global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
 [assembly: Uno.Extensions.Reactive.Config.BindableGenerationTool(3)]
