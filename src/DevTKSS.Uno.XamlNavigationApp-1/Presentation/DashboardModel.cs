6d394576407e35171dd03aeb03099c4f6ebd05e8
DevTKSS <info@technische-konstruktion.com>
2025-07-30 16:12:15 +0200

    feat(ListState): Add ListState and ListView code from tutorial

--- src/DevTKSS.Uno.XamlNavigationApp-1/Presentation/DashboardModel.cs
+++ src/DevTKSS.Uno.XamlNavigationApp-1/Presentation/DashboardModel.cs
@@ -5,5 +5,20 @@ public partial record DashboardModel
     {
         
     }
+    private async ValueTask<IImmutableList<string>> GetMembers(CancellationToken ct) => _listMembers;
+    
+    private readonly IImmutableList<string> _listMembers = ImmutableList.Create(
+        [
+            "Hans",
+            "Lisa",
+            "Anke",
+            "Tom"
+        ]);
+
     public IState<string> DashboardTitle => State<string>.Value(this, () => "Hallo vom Dashboard");
+
+    public IListState<string> DashboardList => ListState.Async(this,GetMembers)
+                                                        .Selection(SelectedMember);
+
+    public IState<string> SelectedMember => State<string>.Empty(this);
 }
